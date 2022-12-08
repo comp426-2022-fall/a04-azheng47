33e05b04da4f463886ff1768dbb8ed22aef1a9fa
Andrew Zheng <azswim@live.unc.edu>
2022-12-08 15:28:07 -0500

    endpoint for /app/roll/:sides/

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ app.get('/app/roll/', (req, res) => {
 	res.status(200).send(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls)));
 })
 
+app.get('/app/roll/:sides/', (req, res) => {
+	res.status(200).send(roll(req.params.sides, 2, 1));
+})
+
 app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
 	res.status(200).send(roll(req.params.sides, req.params.dice, req.params.rolls));
 })
